7eb0cae2516accb7f3ede751fd24c55aa08a22df
Misty Becker <mistyb01@uw.edu>
2023-05-24 21:28:33 -0700

    added fade-in transitions to sections

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
+
+
 const sectionObserver = new IntersectionObserver((sections)=>{
+    
     sections.forEach(section => {
-        console.log("section", section);
-        let sectionId = section.target.getAttribute("id");
-        let sectionLink = document.getElementById(`${sectionId}-link`);
-        if (section.isIntersecting) {
-            console.log("intersect!")
+        if (document.documentElement.clientWidth <= 768) {
             section.target.classList.add("active");
-            sectionLink.classList.add("highlight");
         } else {
-            console.log("not")
-            section.target.classList.remove("active");
-            sectionLink.classList.remove("highlight");
+            let sectionId = section.target.getAttribute("id");
+            let sectionLink = document.getElementById(`${sectionId}-link`);
+            if (section.isIntersecting) {
+                section.target.classList.add("active");
+                sectionLink.classList.add("highlight");
+            } else {
+                section.target.classList.remove("active");
+                sectionLink.classList.remove("highlight");
+            }
         }
     })
 }, {threshold: 0.5})
 
 // apply an observer to every 'entry' section
+
 document.querySelectorAll('.container').forEach((i) => {
     if (i) {
         sectionObserver.observe(i);
     }
 })
+
