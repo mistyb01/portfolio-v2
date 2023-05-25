d30e9771214eb0d4250f984c38d996d5cf1b8bfc
Misty Becker <mistyb01@uw.edu>
2023-05-25 11:02:51 -0700

    edited intersection observers

--- script.js
+++ script.js
@@ -6,24 +6,42 @@ const sectionObserver = new IntersectionObserver((sections)=>{
         if (document.documentElement.clientWidth <= 768) {
             section.target.classList.add("active");
         } else {
+            if (section.isIntersecting) {
+                section.target.classList.add("active");
+            } else {
+                section.target.classList.remove("active");
+            }
+        }
+    })
+}, {threshold: 0.25})
+
+// note! padding is included in each section, which can make the threshold misleading 
+
+const sectionObserverForLinks = new IntersectionObserver((sections)=>{
+    
+    sections.forEach(section => {
             let sectionId = section.target.getAttribute("id");
             let sectionLink = document.getElementById(`${sectionId}-link`);
             if (section.isIntersecting) {
-                section.target.classList.add("active");
                 sectionLink.classList.add("highlight");
             } else {
                 // section.target.classList.remove("active");
                 sectionLink.classList.remove("highlight");
             }
-        }
     })
 }, {threshold: 0.5})
 
-// apply an observer to every 'entry' section
+// ^ not ideal.. i want a lower threshold, w/o having multiple links highlighted at once
+
+// idea: links have a different threshold than the sections?
+// like, for a link to be highlighted, that needs a 0.5 threshold 
+// (so only one is highlighted at a time!)
+// but the sections toggling their active class should have a lower threshold, like 0.25
 
 document.querySelectorAll('.container').forEach((i) => {
     if (i) {
         sectionObserver.observe(i);
+        sectionObserverForLinks.observe(i);
     }
 })
 
