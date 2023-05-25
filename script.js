1539aca4a495436bcc3fc8a31a6ecafe524ffe0a
Misty Becker <mistyb01@uw.edu>
2023-05-25 13:51:01 -0700

    bugfix: margin-collapse on fixed header

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-
+// there are two observers, bc sections and links need different thresholds..
+// but there may be a better way to do this!
 
 const sectionObserver = new IntersectionObserver((sections)=>{
-    
     sections.forEach(section => {
         if (document.documentElement.clientWidth <= 768) {
             section.target.classList.add("active");
         } else {
             if (section.isIntersecting) {
                 section.target.classList.add("active");
-            } else {
-                section.target.classList.remove("active");
-            }
+            } 
         }
     })
 }, {threshold: 0.25})
 
-// note! padding is included in each section, which can make the threshold misleading 
+// note! padding is included in each section, which can make the threshold misleading ..
+// ( should probably find a better way to space out sections.. )
 
 const sectionObserverForLinks = new IntersectionObserver((sections)=>{
-    
     sections.forEach(section => {
             let sectionId = section.target.getAttribute("id");
             let sectionLink = document.getElementById(`${sectionId}-link`);
@@ -30,13 +28,6 @@ const sectionObserverForLinks = new IntersectionObserver((sections)=>{
             }
     })
 }, {threshold: 0.5})
-
-// ^ not ideal.. i want a lower threshold, w/o having multiple links highlighted at once
-
-// idea: links have a different threshold than the sections?
-// like, for a link to be highlighted, that needs a 0.5 threshold 
-// (so only one is highlighted at a time!)
-// but the sections toggling their active class should have a lower threshold, like 0.25
 
 document.querySelectorAll('.container').forEach((i) => {
     if (i) {
